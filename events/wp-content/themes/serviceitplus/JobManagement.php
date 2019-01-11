dcfbb9aeaac57e43ec72f0632d8554dad6a31d7e
jasoncalora <46363599+jasoncalora@users.noreply.github.com>
2019-01-11 17:26:10 +0800

    asd

--- events/wp-content/themes/serviceitplus/JobManagement.php
+++ events/wp-content/themes/serviceitplus/JobManagement.php
@@ -3,10 +3,22 @@
 Template Name: JobManagement
 */
 session_start();
+//// remove all session variables
+//session_unset(); 
+//
+//// destroy the session 
+//session_destroy(); 
 //echo $_SESSION['sessionOwner'];
 if ($_SESSION['sessionOwner'] == '')
 {
     header('Location: http://desktop-5ctqqt6:8081/serviceitplus/events/login/');
 }
 ?>
-asdasd
+<html>
+    <head>
+        
+    </head>
+<body>
+    <H1>Welcome <?php echo $_SESSION['sessionOwner']; ?></H1>
+</body>
+</html>
