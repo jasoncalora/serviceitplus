bca59a7ae5c1ecc31b6c7eb6a30dd88542f390ed
jasoncalora <46363599+jasoncalora@users.noreply.github.com>
2019-11-05 09:13:02 +0800

    removed events

--- events/wp-content/themes/serviceitplus/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   session_start();
-   unset($_SESSION['sessionOwner']);
-   
-   echo 'You have cleaned session';
-//   header('Refresh: 2; URL = login.php');
-   header("Location: http://desktop-5ctqqt6:8081/serviceitplus/events/careersmgt/");
-?>
